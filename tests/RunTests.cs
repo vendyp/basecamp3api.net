165bf486a700b3cb8fe22c113209cda47a03eea6
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-08-08 23:05:01 +0700

    initial project

---
+++ tests/RunTests.cs
@@ -0,0 +1,13 @@
+namespace Basecamp3Api.Tests;
+
+public class RunTests
+{
+    /// <summary>
+    /// Marker
+    /// </summary>
+    [Fact]
+    public void Run()
+    {
+        true.ShouldBeTrue();
+    }
+}
