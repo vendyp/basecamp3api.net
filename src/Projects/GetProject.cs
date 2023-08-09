03a3b14220de841a8cd1b48618bfb8b2a20721b0
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-08-10 00:14:09 +0700

    add api get project

--- src/Projects/GetProject.cs
+++ src/Projects/GetProject.cs
@@ -2,4 +2,33 @@
 
 public partial class BasecampApiClient
 {
+    public async Task<Project> GetProject(int accountId, long projectId)
+    {
+        if (!TokenHasBeenSet)
+            throw new InvalidOperationException("Token has not been set");
+
+        if (!AccountHasBeenSet)
+            throw new InvalidOperationException("Account has not been set");
+
+        if (!Accounts.Any(e => e.Id == accountId))
+            throw new ArgumentException("Invalid account id", nameof(accountId));
+
+        //$ACCOUNT_ID/projects/1.json
+        string pattern = $"{accountId}/projects/{projectId}.json";
+        var uri = new UriBuilder(BaseUrl + pattern);
+
+        var request = new HttpRequestMessage(HttpMethod.Get, uri.ToString());
+        request.Headers.Authorization = new AuthenticationHeaderValue("bearer", AccessToken);
+        request.Headers.Add("User-Agent", $"Basecamp 4 Library ({_setting.RedirectUrl})");
+        var response = await _httpClient.SendAsync(request, CancellationToken.None);
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        if (response.StatusCode != HttpStatusCode.OK)
+            throw new Exception("Result not OK when Get project", new Exception($"With message : {content}"));
+
+        var result = JsonSerializer.Deserialize<Project>(content)!;
+
+        return result;
+    }
 }
---
+++ tests/Projects/GetProjectTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Basecamp3Api.Tests.Projects;
+
+[Collection(nameof(BaseFixture))]
+public class GetProjectTests
+{
+    private readonly BaseFixture _baseFixture;
+
+    public GetProjectTests(BaseFixture baseFixture)
+    {
+        _baseFixture = baseFixture;
+    }
+
+    [Fact]
+    public async Task GetProject_Should_Do_As_Expected()
+    {
+        var auth = await _baseFixture.Client.GetAuthorizationAsync();
+        auth.ShouldNotBeNull();
+
+        var projects = await _baseFixture.Client.GetAllProject(auth.Accounts!.First().Id, 1);
+        projects.Results.Count.ShouldBePositive();
+
+        var project = await _baseFixture.Client.GetProject(auth.Accounts!.First().Id, projects.Results.First().Id);
+        project.ShouldNotBeNull();
+        projects.Results.First().Id.ShouldBe(project.Id);
+    }
+}
