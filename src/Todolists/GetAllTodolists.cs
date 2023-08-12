ffa6a7e84496f46d1df2bd8c038b98619037616e
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-08-12 09:42:46 +0700

    Add new API GetAllTodolists

---
+++ src/Todolists/GetAllTodolists.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Specialized;
+
+namespace Basecamp3Api;
+
+public partial class BasecampApiClient
+{
+    public async Task<(PagedList<Todoset>? TodoLists, Error? Error)> GetAllTodolistsAsync(
+        long accountId,
+        long projectId,
+        long todosetId,
+        TodolistOptions? options,
+        CancellationToken cancellationToken = default)
+    {
+        if (!TokenHasBeenSet)
+            return (null, new Error
+            {
+                StatusCode = -1,
+                Message = "Token has not been set"
+            });
+
+        if (!AccountHasBeenSet)
+            return (null, new Error
+            {
+                StatusCode = -1,
+                Message = "Account has not been set"
+            });
+
+        if (!Accounts.Any(e => e.Id == accountId))
+            return (null, new Error
+            {
+                StatusCode = -1,
+                Message = "Invalid account id"
+            });
+
+        options ??= new TodolistOptions();
+
+        var pattern = $"{accountId}/buckets/{projectId}/todosets/{todosetId}/todolists.json";
+        var nvc = new NameValueCollection(2)
+        {
+            ["page"] = options.Page.ToString()
+        };
+        if (options.Status.HasValue && options.Status.Value != Status.Active)
+            nvc["status"] = nameof(options.Status.Value).ToLower();
+
+        var uriBuilder = new UriBuilder(BaseUrl + pattern);
+        uriBuilder.AddQueryParams(ConstructQueryString(nvc));
+
+        var request = CreateRequestMessageWithAuthentication(HttpMethod.Get, uriBuilder.Uri, null);
+
+        var response = await SendMessageAsync(request, HttpStatusCode.OK, cancellationToken);
+
+        if (response.Error != null)
+            return (null, response.Error);
+
+        var results = JsonSerializer.Deserialize<List<Todoset>>(response.Response!.Value.ResultJsonInString)!;
+
+        return (new PagedList<Todoset>(results)
+            {
+                HasNextPage = response.Response.Value.Headers.Any(e => e.Key == "Link")
+            },
+            null);
+    }
+}
+
+public record TodolistOptions
+{
+    public int Page { get; set; } = 1;
+    public Status? Status { get; set; }
+}
+
+public enum Status
+{
+    Trashed,
+
+    Active,
+
+    Archived
+}
---
+++ tests/Todolists/GetAllTodolistsTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Basecamp3Api.Tests.Todolists;
+
+[Collection(nameof(BaseFixture))]
+public class GetAllTodolistsTests
+{
+    private readonly BaseFixture _baseFixture;
+
+    public GetAllTodolistsTests(BaseFixture baseFixture)
+    {
+        _baseFixture = baseFixture;
+    }
+
+    [Fact]
+    public async Task GetAllTodolists_Should_Do_As_Expected()
+    {
+        var todolists = await _baseFixture.Client.GetAllTodolistsAsync(
+            _baseFixture.AccountId,
+            _baseFixture.ProjectId,
+            _baseFixture.DockTodosetsId,
+            null, CancellationToken.None);
+
+        todolists.Error.ShouldBeNull();
+        todolists.TodoLists.ShouldNotBeNull();
+    }
+}
