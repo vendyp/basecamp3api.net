92ff1c24b1a3b56ca2d8596dc919a0d0813fb85e
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-08-14 20:03:11 +0700

    add new API Get All todos by todolistid

--- src/Todos/GetAllTodos.cs
+++ src/Todos/GetAllTodos.cs
@@ -1,9 +1,60 @@
-﻿namespace Basecamp3Api;
+﻿using System.Collections.Specialized;
+
+namespace Basecamp3Api;
 
 public partial class BasecampApiClient
 {
-    public Task<(PagedList<Todos>? List, Error? Error)> GetAllTodos()
+    public async Task<(PagedList<Todos>? List, Error? Error)> GetAllTodos(long accountId,
+        long projectId,
+        long todolistsId,
+        GetAllTodosOption options,
+        CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        if (!TokenHasBeenSet)
+            return (null, new Error
+            {
+                StatusCode = -1,
+                Message = "Token has not been set"
+            });
+
+        var err = ValidateAccount(accountId);
+        if (err != null)
+            return (null, err);
+
+        var nvc = new NameValueCollection(3)
+        {
+            ["page"] = options.Page.ToString()
+        };
+        if (options.Status.HasValue && options.Status.Value != Status.Active)
+            nvc["status"] = nameof(options.Status.Value).ToLower();
+
+        if (options.Completed.HasValue)
+            nvc["completed"] = options.Completed.Value.ToString().ToLower();
+
+        // GET /buckets/1/todolists/3/todos.json
+        var pattern = $"{accountId}/buckets/{projectId}/todolists/{todolistsId}/todos.json";
+        var uriBuilder = new UriBuilder(BaseUrl + pattern);
+        uriBuilder.AddQueryParams(ConstructQueryString(nvc));
+
+        var request = CreateRequestMessageWithAuthentication(HttpMethod.Get,
+            uriBuilder.Uri, null);
+
+        var response = await SendMessageAsync(request, HttpStatusCode.OK, cancellationToken);
+
+        if (response.Error != null)
+            return (null, response.Error);
+
+        var result = JsonSerializer.Deserialize<List<Todos>>(response.Response!.Value.ResultJsonInString)!;
+
+        return (
+            new PagedList<Todos>(result) { HasNextPage = response.Response.Value.Headers.Any(e => e.Key == "Link") },
+            null);
     }
+}
+
+public record GetAllTodosOption
+{
+    public int Page { get; set; } = 1;
+    public Status? Status { get; set; }
+    public bool? Completed { get; set; }
 }
---
+++ tests/Todos/GetAllTodosTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Basecamp3Api.Tests.Todos;
+
+[Collection(nameof(Basecamp3ApiTestCollection))]
+public class GetAllTodosTests
+{
+    private readonly BaseFixture _baseFixture;
+
+    public GetAllTodosTests(BaseFixture baseFixture)
+    {
+        _baseFixture = baseFixture;
+    }
+
+    [Fact]
+    public async Task GetAllTodos_Should_Do_As_Expected()
+    {
+        var resultList = await _baseFixture.Client.GetAllTodos(
+            _baseFixture.AccountId,
+            _baseFixture.ProjectId,
+            6444832541,
+            new GetAllTodosOption { Page = 1 });
+
+        resultList.Error.ShouldBeNull();
+        resultList.List.ShouldNotBeNull();
+    }
+}
