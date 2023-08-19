427dbe77ae1c6e3ec3a98c7da737b467cdc807a2
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-08-19 10:00:31 +0700

    add get all people by account api

---
+++ src/Peoples/GetAllPeople.cs
@@ -0,0 +1,35 @@
+﻿namespace Basecamp3Api;
+
+public partial class BasecampApiClient
+{
+    public async Task<(List<People>? Peoples, Error? Error)> GetAllPeopleAsync(
+        long accountId,
+        CancellationToken cancellationToken)
+    {
+        if (!TokenHasBeenSet)
+            return (null, new Error
+            {
+                StatusCode = -1,
+                Message = "Token has not been set"
+            });
+
+        var err = ValidateAccount(accountId);
+        if (err != null)
+            return (null, err);
+
+        //$ACCOUNT_ID/projects/.json
+        var pattern = $"{accountId}/people.json";
+        var uri = new Uri(BaseUrl + pattern);
+
+        var request = CreateRequestMessageWithAuthentication(HttpMethod.Get, uri, null);
+
+        var response = await SendMessageAsync(request, HttpStatusCode.OK, cancellationToken);
+
+        if (response.Error != null)
+            return (null, response.Error);
+
+        var result = JsonSerializer.Deserialize<List<People>>(response.Response!.Value.ResultJsonInString)!;
+
+        return (new List<People>(result), null);
+    }
+}
---
+++ tests/Peoples/GetAllPeopleTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Basecamp3Api.Tests.Peoples;
+
+[Collection(nameof(Basecamp3ApiTestCollection))]
+public class GetAllPeopleTests
+{
+    private readonly BaseFixture _baseFixture;
+
+    public GetAllPeopleTests(BaseFixture baseFixture)
+    {
+        _baseFixture = baseFixture;
+    }
+
+    [Fact]
+    public async Task GetAllPeople_Should_Do_As_Expected()
+    {
+        var results = await _baseFixture.Client.GetAllPeopleAsync(
+            _baseFixture.AccountId,
+            CancellationToken.None);
+
+        results.Error.ShouldBeNull();
+        results.Peoples.ShouldNotBeNull();
+    }
+}
