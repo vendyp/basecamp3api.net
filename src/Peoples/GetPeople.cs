6d26ee34f9d24d45f8367e1b12ab48d962d41c86
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-08-19 10:09:15 +0700

    add new api get people

---
+++ src/Peoples/GetPeople.cs
@@ -0,0 +1,36 @@
+﻿namespace Basecamp3Api;
+
+public partial class BasecampApiClient
+{
+    public async Task<(People? People, Error? Error)> GetPeopleAsync(
+        long accountId,
+        long personId,
+        CancellationToken cancellationToken)
+    {
+        if (!TokenHasBeenSet)
+            return (null, new Error
+            {
+                StatusCode = -1,
+                Message = "Token has not been set"
+            });
+
+        var err = ValidateAccount(accountId);
+        if (err != null)
+            return (null, err);
+
+        //$ACCOUNT_ID/projects/.json
+        var pattern = $"{accountId}/people/{personId}.json";
+        var uri = new Uri(BaseUrl + pattern);
+
+        var request = CreateRequestMessageWithAuthentication(HttpMethod.Get, uri, null);
+
+        var response = await SendMessageAsync(request, HttpStatusCode.OK, cancellationToken);
+
+        if (response.Error != null)
+            return (null, response.Error);
+
+        var result = JsonSerializer.Deserialize<People>(response.Response!.Value.ResultJsonInString)!;
+
+        return (result, null);
+    }
+}
---
+++ tests/Peoples/GetPeopleTests.cs
@@ -0,0 +1,37 @@
+﻿namespace Basecamp3Api.Tests.Peoples;
+
+[Collection(nameof(Basecamp3ApiTestCollection))]
+public class GetPeopleTests
+{
+    private readonly BaseFixture _baseFixture;
+
+    public GetPeopleTests(BaseFixture baseFixture)
+    {
+        _baseFixture = baseFixture;
+    }
+
+    [Fact]
+    public async Task GetPeople_Should_Do_As_Expected()
+    {
+        var results = await _baseFixture.Client.GetAllPeopleAsync(
+            _baseFixture.AccountId,
+            CancellationToken.None);
+
+        results.Error.ShouldBeNull();
+        results.Peoples.ShouldNotBeNull();
+
+
+        var first = results.Peoples.FirstOrDefault();
+        if (first is null)
+            return;
+
+        var resultPeople = await _baseFixture.Client.GetPeopleAsync(
+            _baseFixture.AccountId,
+            first.Id,
+            CancellationToken.None);
+
+        resultPeople.Error.ShouldBeNull();
+        resultPeople.People.ShouldNotBeNull();
+        resultPeople.People.Id.ShouldBe(first.Id);
+    }
+}
