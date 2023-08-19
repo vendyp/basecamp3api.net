3ea670227047148271705415e51494c31c826172
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-08-19 09:57:14 +0700

    add new api GetAllPeopleInProject

---
+++ src/Peoples/GetPeopleInProject.cs
@@ -0,0 +1,36 @@
+﻿namespace Basecamp3Api;
+
+public partial class BasecampApiClient
+{
+    public async Task<(List<People>? Peoples, Error? Error)> GetAllPeopleInProjectAsync(
+        long accountId,
+        long projectId,
+        CancellationToken cancellationToken)
+    {
+        if (!TokenHasBeenSet)
+            return (null, new Error
+            {
+                StatusCode = -1,
+                Message = "Token has not been set"
+            });
+
+        var err = ValidateAccount(accountId);
+        if (err != null)
+            return (null, err);
+
+        //$ACCOUNT_ID/projects/.json
+        var pattern = $"{accountId}/projects/{projectId}/people.json";
+        var uri = new Uri(BaseUrl + pattern);
+
+        var request = CreateRequestMessageWithAuthentication(HttpMethod.Get, uri, null);
+
+        var response = await SendMessageAsync(request, HttpStatusCode.OK, cancellationToken);
+
+        if (response.Error != null)
+            return (null, Error: response.Error);
+
+        var result = JsonSerializer.Deserialize<List<People>>(response.Response!.Value.ResultJsonInString)!;
+
+        return (new List<People>(result), null);
+    }
+}
---
+++ tests/Peoples/GetAllPeopleInProjectTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Basecamp3Api.Tests.Peoples;
+
+[Collection(nameof(Basecamp3ApiTestCollection))]
+public class GetAllPeopleInProjectTests
+{
+    private readonly BaseFixture _baseFixture;
+
+    public GetAllPeopleInProjectTests(BaseFixture baseFixture)
+    {
+        _baseFixture = baseFixture;
+    }
+
+    [Fact]
+    public async Task GetAllPeopleInProject_Should_Do_As_Expected()
+    {
+        var todolists = await _baseFixture.Client.GetAllPeopleInProjectAsync(
+            _baseFixture.AccountId,
+            _baseFixture.ProjectId,
+            CancellationToken.None);
+
+        todolists.Error.ShouldBeNull();
+        todolists.Peoples.ShouldNotBeNull();
+    }
+}
