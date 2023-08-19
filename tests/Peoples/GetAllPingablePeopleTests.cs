f5addcb020218c72043a223721921da223d5fdb3
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-08-19 10:05:22 +0700

    add new api get all pingable people by account id

---
+++ tests/Peoples/GetAllPingablePeopleTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Basecamp3Api.Tests.Peoples;
+
+[Collection(nameof(Basecamp3ApiTestCollection))]
+public class GetAllPingablePeopleTests
+{
+    private readonly BaseFixture _baseFixture;
+
+    public GetAllPingablePeopleTests(BaseFixture baseFixture)
+    {
+        _baseFixture = baseFixture;
+    }
+
+    [Fact]
+    public async Task GetPingablePeople_Should_Do_As_Expected()
+    {
+        var results = await _baseFixture.Client.GetAllPingablePeopleAsync(
+            _baseFixture.AccountId,
+            CancellationToken.None);
+
+        results.Error.ShouldBeNull();
+        results.Peoples.ShouldNotBeNull();
+    }
+}
