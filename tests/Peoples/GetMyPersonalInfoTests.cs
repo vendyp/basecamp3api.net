cb48e06c87e3634aca64b6a8fd737b1d58f6106d
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-08-19 10:56:33 +0700

    add new api get my personal info

---
+++ tests/Peoples/GetMyPersonalInfoTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Basecamp3Api.Tests.Peoples;
+
+[Collection(nameof(Basecamp3ApiTestCollection))]
+public class GetMyPersonalInfoTests
+{
+    private readonly BaseFixture _baseFixture;
+
+    public GetMyPersonalInfoTests(BaseFixture baseFixture)
+    {
+        _baseFixture = baseFixture;
+    }
+
+    [Fact]
+    public async Task GetMyPersonalInfo_Should_Do_As_Expected()
+    {
+        var result = await _baseFixture.Client.GetMyPersonalInfoAsync(
+            _baseFixture.AccountId,
+            CancellationToken.None);
+
+        result.Error.ShouldBeNull();
+        result.People.ShouldNotBeNull();
+    }
+}
