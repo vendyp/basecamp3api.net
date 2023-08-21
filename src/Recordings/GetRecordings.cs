9cb1c0ab0881bbe08387b29202bc614d6b6395ff
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-08-21 23:48:23 +0700

    add boiler api for get recordings

---
+++ src/Recordings/GetRecordings.cs
@@ -0,0 +1,46 @@
+﻿namespace Basecamp3Api;
+
+public partial class BasecampApiClient
+{
+    public Task<(PagedList<object>? Response, Error? Error)> GetRecordingsAsync(
+        int page,
+        GetRecordingType type,
+        long[]? projectIds,
+        GetRecodingStatus status = GetRecodingStatus.Active,
+        GetRecordingSort sort = GetRecordingSort.Created,
+        GetRecordingDirection direction = GetRecordingDirection.Descending,
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public enum GetRecodingStatus
+    {
+        Active,
+
+        Archived,
+
+        Trashed
+    }
+
+    public enum GetRecordingType
+    {
+        Todo,
+
+        Todolist,
+    }
+
+    public enum GetRecordingSort
+    {
+        Created,
+
+        Updated
+    }
+
+    public enum GetRecordingDirection
+    {
+        Ascending,
+
+        Descending
+    }
+}
