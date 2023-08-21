aa2675f2bc3cc9f791c9d4ad4acec38c10e35ceb
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2023-08-21 22:21:06 +0700

    add boilerapi for archive/trash/unarchive recordings

---
+++ src/Recordings/ArchiveRecording.cs
@@ -0,0 +1,13 @@
+﻿namespace Basecamp3Api.Recordings;
+
+public partial class BasecampApiClient
+{
+    public Task<Error?> ArchiveRecordingAsync(
+        long accountId,
+        long projectId,
+        long id,
+        CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
